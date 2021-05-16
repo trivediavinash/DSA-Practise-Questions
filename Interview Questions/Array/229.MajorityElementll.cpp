da5c452b9dcfab81077ac07aab82d135b048678c
“trivediavinash” <trivedi.avinash02@gmail.com>
2021-05-16 18:48:24 +0530

    codes

---
+++ Interview Questions/Array/229.MajorityElementll.cpp
@@ -0,0 +1,89 @@
+/*
+
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: [3]
+Example 2:
+
+Input: nums = [1]
+Output: [1]
+Example 3:
+
+Input: nums = [1,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-109 <= nums[i] <= 109
+
+*/
+
+
+//can be done with n square complexity checking each and every element and checking their freq count .
+
+1. O(N)  O(N)
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        map<int, int> um;
+        for(int i=0;i<nums.size();i++){
+            um[nums[i]]++;
+        }
+        for(auto it=um.begin();it!=um.end();++it){
+            if(it->second>(nums.size()/3))  ans.push_back(it->first);
+        }
+        return ans;
+    }
+};
+
+2. Boyer Moore's voting algorithm  O(N)  O(1)
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        int candidate1=nums[0], candidate2=nums[0], count1=0, count2=0;
+        for(int num:nums){
+            if(num==candidate1) count1++;
+            else if(num==candidate2) count2++;
+            else if(count1==0){
+                candidate1=num;
+                count1++;
+            }
+             else if(count2==0){
+                candidate2=num;
+                count2++;
+            }
+            else {
+                count1--;
+                count2--;
+            }
+        }
+            count1=0;
+            count2=0;
+            for(int num:nums){
+                if(num==candidate1){
+                    count1++;
+                }
+                else if(num==candidate2){
+                    count2++;
+                }
+            }
+            if(count1>nums.size()/3)    ans.push_back(candidate1);
+            else if(count2>nums.size()/3)   ans.push_back(candidate2);
+            
+       
+        return ans;
+    }
+};
