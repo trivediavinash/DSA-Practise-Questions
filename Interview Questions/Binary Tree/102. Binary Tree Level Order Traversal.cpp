d0af7c72ad2e3065302424e3d772aaa4515fde0c
Avinash Trivedi <trivedi.avinash02@gmail.com>
2021-08-06 10:39:11 +0530

    codes

--- Interview Questions/Binary Tree/102. Binary Tree Level Order Traversal.cpp
+++ Interview Questions/Binary Tree/102. Binary Tree Level Order Traversal.cpp
@@ -26,8 +26,9 @@ return its level order traversal as:
 ]
 
 */
-
-1.//  T.C.=(N) S.C.= o(1) for tree having only one child (like linklist) )   (N) ,for perefect tree it will have n/2 nodes in last level ,occupy the same numbr of nodes in the queue somewhere .  
+DFS
+1.//  T.C.=(N) S.C. = o(1) for tree having only one child (like linklist)   
+//S.C.= O(N) ,for perefect tree it will have n/2 nodes in last level ,occupy the same numbr of nodes in the queue somewhere .  
 
 class Solution {
 public:
@@ -58,4 +59,32 @@ public:
     }
 };
 
+2. BFS  
+// T.c. = O(N), S.c. = O(LogN) - for full tree
 
+// S.c. = O(N) for skewed tree link linklist .  
+
+//vector < vector < int >> res; we declare it globally if we do not wamt to pass it to funtion(dfs) each time. 
+class Solution {
+  private:
+    void dfs(vector < vector < int >> & res, TreeNode * root, int level) {
+      if (root == NULL) return; //if NULL just return 
+      if (level == res.size()) res.push_back({}); //pushing new vector for each level .
+
+      res[level].push_back(root -> val); //pushing value of root 
+      dfs(res, root -> left, level + 1); //calling for left subpart and right subpart 
+      dfs(res, root -> right, level + 1);
+    }
+  public:
+    vector < vector < int >> levelOrder(TreeNode * root) {
+      vector < vector < int >> res;
+      dfs(res, root, 0);
+      return res;
+    }
+};
+
+/*
+there are tradeoffs,
+for very 'lean' tree (most non-leaf node have only one child), this dfs appoach consume O(n) memory, while bfs approach with queue cost almost constant space.
+for near complete tree (most non-leaf node have two child), dfs approach cost O(log(n)) memory, whereas bfs approach cost O(n) memory
+*/
